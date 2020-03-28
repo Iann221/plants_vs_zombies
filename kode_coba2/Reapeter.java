579eb8cc41310abcc7fa7e13a58db02c1f0d1101
rfaturr <54592012+rfaturr@users.noreply.github.com>
2020-03-28 19:47:38 +0700

    Add files via upload

---
+++ kode_coba2/Reapeter.java
@@ -0,0 +1,11 @@
+public class Reapeter extends Plant{
+    // constructor
+
+    public Reapeter(){
+        this.name = 'Rp';
+        //this.x = ;
+        //this.y = ;
+        this.speed = 2;
+        this.health = 5;
+    }
+}
